8289615fc21044d276b6ab9b7fac7238268ba6bc
JPCenz <p.cenzano5@gmail.com>
2024-08-07 22:06:04 -0500

    Initial commit

---
+++ src/main/java/com/jpcenz/projects/springwebfluxeurekaserver/SpringWebfluxEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.jpcenz.projects.springwebfluxeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+
+public class SpringWebfluxEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpcenz/projects/springwebfluxeurekaserver/SpringWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpcenz.projects.springwebfluxeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
